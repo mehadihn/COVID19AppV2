c9a3ca9a5e0dc7c325d40836707c8412fe11caab
Mehadi Hassan <mehadihassan116@gmail.com>
2020-05-31 02:24:03 +0600

    env

---
+++ app/src/main/java/com/noobs/impactsofcovid_19/air.java
@@ -0,0 +1,14 @@
+package com.noobs.impactsofcovid_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class air extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_air);
+    }
+}
--- app/src/main/java/com/noobs/impactsofcovid_19/env.java
+++ app/src/main/java/com/noobs/impactsofcovid_19/env.java
@@ -2,13 +2,63 @@ package com.noobs.impactsofcovid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class env extends AppCompatActivity {
+    private ImageView backbutton;
+    private LinearLayout air, light, water;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_env);
+
+        backbutton = findViewById(R.id.backbutton);
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(env.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        air = findViewById(R.id.air);
+
+        air.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(env.this, air.class);
+                startActivity(i);
+            }
+        });
+
+        water = findViewById(R.id.water);
+
+        water.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(env.this, water.class);
+                startActivity(i);
+            }
+        });
+
+        light = findViewById(R.id.light);
+
+        light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(env.this, light.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
