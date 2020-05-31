98105de080fc27eb1ce84a1f8c74a3a713e2ed32
Mehadi Hassan <mehadihassan116@gmail.com>
2020-05-31 16:08:19 +0600

    ind

--- app/src/main/java/com/noobs/impactsofcovid_19/Industry.java
+++ app/src/main/java/com/noobs/impactsofcovid_19/Industry.java
@@ -3,11 +3,16 @@ package com.noobs.impactsofcovid_19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
 public class Industry extends AppCompatActivity {
+    ImageView p1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,15 @@ public class Industry extends AppCompatActivity {
         });
 
 
+        p1 = findViewById(R.id.p1);loadImg();
+    }
 
+    private void loadImg(){
+
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.down)
+                .error(R.drawable.fail);
+
+        Glide.with(this).load("https://static.scientificamerican.com/blogs/cache/file/9C0D6895-2556-4662-B3C5DA872E24CF8E_source.jpg").apply(options).into(p1);
     }
 }
